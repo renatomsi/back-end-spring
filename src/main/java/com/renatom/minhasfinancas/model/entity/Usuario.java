f82a79eb843eea439dacf24321b265af868a6041
Renato Marques <renatomarques.contato@gmail.com>
2022-05-24 19:53:27 -0300

    First Commit

---
+++ src/main/java/com/renatom/minhasfinancas/model/entity/Usuario.java
@@ -0,0 +1,37 @@
+package com.renatom.minhasfinancas.model.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table( name = "usuario" , schema = "financas")
+// Cria todos os get and set , equals and hashCodes
+@Data
+@Builder
+public class Usuario {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "senha")
+	private String senha;
+
+	
+	
+}
---
+++ src/main/java/com/renatom/minhasfinancas/model/enums/StatusLancamento.java
@@ -0,0 +1,9 @@
+package com.renatom.minhasfinancas.model.enums;
+
+public enum StatusLancamento {
+	
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+	
+}
