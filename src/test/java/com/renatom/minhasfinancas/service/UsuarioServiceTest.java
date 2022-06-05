ab626b4d0659dde7f8a29d3864ac986d4c4831b9
Renato Marques <renatomarques.contato@gmail.com>
2022-06-05 12:02:18 -0300

    51-Finalizando os testes na camada de Serviço para os usuarios

--- src/test/java/com/renatom/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/renatom/minhasfinancas/service/UsuarioServiceTest.java
@@ -2,25 +2,26 @@ package com.renatom.minhasfinancas.service;
 
 import java.util.Optional;
 
-import org.aspectj.lang.annotation.Before;
+
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.renatom.minhasfinancas.exception.ErroAutenticacao;
+import com.renatom.minhasfinancas.exception.RegraNegocioException;
 import com.renatom.minhasfinancas.model.entity.Usuario;
 import com.renatom.minhasfinancas.model.repository.UsuarioRepository;
 import com.renatom.minhasfinancas.service.impl.UsuarioServiceImpl;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+@RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 	
@@ -30,15 +31,47 @@ public class UsuarioServiceTest {
 //	Criado classe setUp para setar o repository como uma classe fake e passando para o service o repository fake.
 //	Chamando a anotação @MockBean ela transforma o repository em uma classe fake
 
-	
-	UsuarioService service;
+	@SpyBean
+	UsuarioServiceImpl service;
 	
 	@MockBean
 	UsuarioRepository repository;
 	
-//	@Before(value = "teste")
-	public void setUp() {
-		service = new UsuarioServiceImpl(repository);
+	@Test
+	public void deveSalvarUsuario() {
+		//cenario
+		Mockito.doNothing().when(service).validarEmail(Mockito.anyString());
+		Usuario usuario = Usuario.builder().id(1l).name("nome").email("email@email.com").senha("senha").build();
+		
+		Mockito.when(repository.save(Mockito.any(Usuario.class))).thenReturn(usuario);
+		
+		//acao
+		Usuario usuarioSalvo = service.salvarUsuario(new Usuario());
+		
+		// verificação 
+		Assertions.assertThat(usuarioSalvo).isNotNull();
+		Assertions.assertThat(usuarioSalvo.getId()).isEqualTo(1l);
+		Assertions.assertThat(usuarioSalvo.getName()).isEqualTo("nome");
+		Assertions.assertThat(usuarioSalvo.getEmail()).isEqualTo("email@email.com");
+		Assertions.assertThat(usuarioSalvo.getSenha()).isEqualTo("senha");
+		
+		
+	}
+	
+	@Test(expected = RegraNegocioException.class)
+	public void naoDeveSalvarUmUsuarioComEmailJaCadastrado() {
+		// cenario
+		String  email = "email@email.com";
+		Usuario usuario = Usuario.builder().email(email).build();
+		// Retorna uma regra de negocio exception ao tentar validar um email ja cadastrado.
+		Mockito.doThrow(RegraNegocioException.class).when(service).validarEmail(email);
+		
+		//acao
+		service.salvarUsuario(usuario);
+		
+		// verificação
+		// Verifica se o metodo repository.save nunca foi chamado.
+		Mockito.verify(repository, Mockito.never()).save(usuario);
 	}
 	
 	
@@ -86,7 +119,7 @@ public class UsuarioServiceTest {
 	}
 	
 	
-	@Test
+	@Test(expected = Test.None.class)
 	public void deveValidarEmail() {
 		//cenario
 //		Esta informando se tiver qualquer string no metodo existsByEmail deve retornar false para que consiga salvar o email.
@@ -96,7 +129,7 @@ public class UsuarioServiceTest {
 		service.validarEmail("email@email.com");
 	}
 	
-	@Test
+	@Test(expected = RegraNegocioException.class)
 	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
 		//cenario 
 //		Esta informando se tiver qualquer string no metodo existsByEmail deve retornar True para que não consiga salvar o email.
@@ -104,6 +137,8 @@ public class UsuarioServiceTest {
 		
 		//acao
 		service.validarEmail("email@email.com");
+		
+		
 	}
 
 }
